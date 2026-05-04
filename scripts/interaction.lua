caba0d603239de21995c7b2539ead640bddae6e3
Mutim <taormino24@gmail.com>
2026-05-04 09:43:14 -0500

    Fix Astrid spawn cue to alert player and set player ownership

--- scripts/interaction.lua
+++ scripts/interaction.lua
@@ -1,18 +1,5 @@
--- Create the main menu object
-local mainMenu = Menu.create()
-
--- Set the title of the menu
-mainMenu.title = "My Mod Menu"
-
--- Add a button to the menu
-local button1 = mainMenu:addButton("Button 1")
-
--- Set the function that will be called when the button is clicked
-button1.onClick = function()
-    print("Button 1 was clicked!")
-end
-
--- Show the menu when Shift+U is pressed
-Input.keyPressed("LShift", "U", function()
-    mainMenu:show()
-end)
+-- Hotkey/spawn behavior is implemented in MD (md/casino.xml)
+-- via the Hotkey API and create_ship actions.
+--
+-- Intentionally left empty to avoid using non-existent Lua APIs like
+-- Input.contextActive / MD.Signal for ship spawning.
